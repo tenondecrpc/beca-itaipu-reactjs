7e5f562c5c82ec7b3293ab3b70f5c83976d0a3ce
Cristian Paniagua <tenondecrpc@gmail.com>
2025-01-13 19:44:47 -0300

    fix(lint): fixed eslint error

--- src/context/QuizProvider.tsx
+++ src/context/QuizProvider.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { ReactNode, useEffect, useState } from 'react'
 import { quiz } from '../data/QuizQuestions'
 import { QuizContextTypes, Result, ScreenTypes } from '../types'
