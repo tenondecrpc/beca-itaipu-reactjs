bc1278e327a16a16f817b3bdcac406481d1f0e77
Cristian Paniagua <tenondecrpc@gmail.com>
2025-01-18 20:26:27 -0300

    feat(link): added link to social media

--- src/components/QuizTopicsScreen/index.tsx
+++ src/components/QuizTopicsScreen/index.tsx
@@ -158,7 +158,9 @@ const QuizTopicsScreen: React.FC = () => {
         <DetailItem>
           <DetailText>
             <DetailText>Colaboradores:</DetailText>
-            Este proyecto fue desarrollado por <strong>Wildo Fariña</strong> y <strong>Cristian Paniagua</strong> como un aporte comunitario.
+            Este proyecto fue desarrollado por <strong>Wildo Fariña</strong> y 
+            <strong><a href="https://www.instagram.com/tenondev_/" target="_blank" rel="noreferrer">Cristian Paniagua</a></strong> 
+            como un aporte comunitario.
           </DetailText>
         </DetailItem>
       </DetailTextContainer>
