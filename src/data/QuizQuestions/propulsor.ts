7b0637da4e67d01f7d6a924f30ec4397957f0cb0
Cristian Paniagua <tenondecrpc@gmail.com>
2025-01-19 10:04:00 -0300

    fix(text): fixed test of propulsor

--- src/data/QuizQuestions/propulsor.ts
+++ src/data/QuizQuestions/propulsor.ts
@@ -1,13 +1,13 @@
 export const propulsor = `
 PROPULSOR DE LA EDUCACIÓN PARAGUAYA
 
-El Congreso Nacional reunido en marzo de 1841 el gobierno consular decretó, el 30 de noviembre de 1841, el establecimiento de la Academia Literaria.El plan de estudios comprendía: Latinidad, Idioma Castellano y Bellas Letras, Filosofía Racional, Teología Dogmática, Historia Eclesiástica y Oratoria Sagrada, inicialmente, solo fue provista las dos primeras de las cátedras mencionadas que se confiaron a los P. P. Marco Antonio Maíz, simultáneamente director del instituto, y José Joaquín Palacios. 
+El Congreso Nacional reunido en marzo de 1841 el gobierno consular decretó, el 30 de noviembre de 1841, el establecimiento de la Academia Literaria. El plan de estudios comprendía: Latinidad, Idioma Castellano y Bellas Letras, Filosofía Racional, Teología Dogmática, Historia Eclesiástica y Oratoria Sagrada, inicialmente, solo fue provista las dos primeras de las cátedras mencionadas que se confiaron a los P. P. Marco Antonio Maíz, simultáneamente director del instituto, y José Joaquín Palacios. 
 
 Don Carlos Antonio López fue el primer presidente Constitucional de Paraguay y gran visionario en temas de educación, pues él fue quien en el siglo XX ya había entendido que la formación académica era clave para el desarrollo de una Nación. Una de las prácticas más reconocidas fue el otorgamiento de becas a un grupo de jóvenes destacados para que pudieran estudiar en el exterior; una visión que comprometía al Estado a hacerse cargo de la formación intelectual de su población para dar respuesta a las problemáticas por las que atravesaba el país.
 
 En 1858, varios compatriotas dejaron Paraguay, con el compromiso de que, a su regreso, desarrollarían e implementarían lo aprendido en beneficio del país y su relacionamiento con los países vecinos; así, los "Becarios de López" serían los artifices del proceso de modernización del país.
 
-Los primeros becarios fueron a Gran Bretaña, los mismos fueronseleccionados del Aula de Filosofía y asumieron el compromiso de ir a prepararse para la carrera diplomática; entre ellos: Juan Crisóstomo Centurión, Gerónimo Pérez, Cándido Bareiro, Andrés Maciel y Gaspar López jóvenes que adquirieron una vasta cultura general y una eficiente formación jurídica. Bareiro llegó a ser Presidente de la República en el año 1878 y publicó cuatro volúmenes de "Memorias", de extraordinaria importancia en la historiografía paraguaya, una novela y diversos artículos, estudios y ensayos.
+Los primeros becarios fueron a Gran Bretaña, los mismos fueron seleccionados del Aula de Filosofía y asumieron el compromiso de ir a prepararse para la carrera diplomática; entre ellos: Juan Crisóstomo Centurión, Gerónimo Pérez, Cándido Bareiro, Andrés Maciel y Gaspar López jóvenes que adquirieron una vasta cultura general y una eficiente formación jurídica. Bareiro llegó a ser Presidente de la República en el año 1878 y publicó cuatro volúmenes de "Memorias", de extraordinaria importancia en la historiografía paraguaya, una novela y diversos artículos, estudios y ensayos.
 
 Compilación de textos extraídos de:
 Fuentes consultadas: https://cmcespedes.wordpress.com
